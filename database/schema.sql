88f46ef2cd74956c10234311f491145440138b8d
Jahniele <160905616+Jahniele@users.noreply.github.com>
2026-04-20 19:48:42 -0500

    Create users and customers tables in schema.sql

---
+++ database/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT,
+  password TEXT
+);
+
+CREATE TABLE customers (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  email TEXT
+);
